b101c2feacee86f115766964a447899a8c14f8fc
Afshaal Zubair <afshaalzubair@outlook.com>
2024-10-02 16:47:40 -0500

    button css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import './App.css'
 import SplineScene from './components/SplineScene'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFileAlt } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,12 +23,15 @@ function App() {
             I'm a freshman studying computer science at UT Austin.
           </span>
           <button className="button1">
+            <FontAwesomeIcon icon={faFileAlt} style={{ marginRight: '10px' }} />
             <span>Resume</span>
           </button>
           <button className="button2">
+            <FontAwesomeIcon icon={faLinkedin} style={{ marginRight: '10px' }} />
             <span>LinkedIn</span>
           </button>
           <button className="button3">
+            <FontAwesomeIcon icon={faGithub} style={{ marginRight: '10px' }} />
             <span>GitHub</span>
           </button>
         </main>
