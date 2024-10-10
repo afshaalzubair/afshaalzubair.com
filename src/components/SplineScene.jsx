1d3b8b39c4dc63c6ceace6579055ceab98fb478b
Afshaal Zubair <afshaalzubair@outlook.com>
2024-10-10 02:18:46 -0500

    update spline scene

--- src/components/SplineScene.jsx
+++ src/components/SplineScene.jsx
@@ -4,7 +4,7 @@ import Spline from '@splinetool/react-spline';
 export default function SplineScene() {
   return (
     <div className="spline-scene">
-      <Spline scene="https://prod.spline.design/PLiNorrYzWPsRzRw/scene.splinecode" />
+      <Spline scene="https://prod.spline.design/BPOy7LMhRFs7rGfE/scene.splinecode" />
     </div>
   );
 }
